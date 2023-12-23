67fab3a6f65d461a5e81341970a6f747729a2c6c
Rahul12347 <2019rahul.maurya@ves.ac.in>
2023-12-23 15:38:49 +0530

    url change

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = 'https://twitter-server-production-9db4.up.railway.app/';
+export const API_BASE_URL = 'https://twitter-server-production-9db4.up.railway.app';
 const jwt = localStorage.getItem("jwt")
 export const api = axios.create({
   baseURL: API_BASE_URL, 
